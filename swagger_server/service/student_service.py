628c5222934ff4b4c49870cf4fa50b05d4c3ba72
Julius <julius.wagt1996@hotmail.com>
2021-02-15 11:31:00 +0100

    added change service v2

--- swagger_server/service/student_service.py
+++ swagger_server/service/student_service.py
@@ -36,7 +36,8 @@ def get_student_by_id(student_id, subject):
         return student
     student = Student.from_dict(student)
     if subject not in student.grades:
-        return student
+        return "Not Found", 404
+        #return student
 
 
 def delete_student(student_id):
