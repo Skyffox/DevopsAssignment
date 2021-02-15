a35c8df2b5e827a3c85b914eebc502aef574c93f
Julius <julius.wagt1996@hotmail.com>
2021-02-15 22:05:47 +0100

    final version

--- swagger_server/service/student_service_MongoDB.py
+++ swagger_server/service/student_service_MongoDB.py
@@ -3,39 +3,43 @@ import logging
 import os
 import tempfile
 
-from tinydb import TinyDB, Query
-from tinydb.middlewares import CachingMiddleware
 from functools import reduce
 import uuid
+
+import pymongo  # package for working with MongoDB
 
 from swagger_server.models import Student
 
 db_dir_path = tempfile.gettempdir()
 db_file_path = os.path.join(db_dir_path, "students.json")
-student_db = TinyDB(db_file_path)
+
+client = pymongo.MongoClient("mongodb://localhost:27017/")
+
+db = client["studentsdb"]
+student_db = db["students"]
+
+with open(db_file_path) as f:
+    file_data = json.load(f)
+
+student_db.insert(file_data)
 
 
 def add_student(student):
     if student.first_name is None or student.last_name is None:
         return 'invalid input', 405
 
-    queries = []
-    query = Query()
-    queries.append(query.first_name == student.first_name)
-    queries.append(query.last_name == student.last_name)
-    query = reduce(lambda a, b: a & b, queries)
-    res = student_db.search(query)
+    res = student_db.find({"first_name": student.first_name, "last_name": student.last_name})
     if res:
         return 'already exists', 409
 
-    doc_id = student_db.insert(student.to_dict())
+    doc_id = student_db.insertOne(student.to_dict())
     student.student_id = doc_id
     return student.student_id, 200
 
 
 def get_student_by_id(student_id, subject):
-    student = student_db.get(doc_id=int(student_id))
-    if not student:
+    student = student_db.find({"doc_id":int(student_id)})
+    if student.count() == 0:
         return "Not Found", 404
 
     student = Student.from_dict(student)
@@ -47,10 +51,9 @@ def get_student_by_id(student_id, subject):
 
 
 def get_student_by_last_name(last_name):
-    query = Query()
-    student = student_db.get(query.last_name == last_name)
+    student = student_db.find({"last_name": last_name})
 
-    if not student:
+    if student.count() == 0:
         return "Not Found", 404
 
     student = Student.from_dict(student)
@@ -58,8 +61,8 @@ def get_student_by_last_name(last_name):
 
 
 def delete_student(student_id):
-    student = student_db.get(doc_id=int(student_id))
-    if not student: 
+    student = student_db.find({"doc_id":int(student_id)})
+    if student.count() == 0:
         return "Not Found", 404
-    student_db.remove(doc_ids=[int(student_id)])
+    student.db.remove({"doc_id":int(student_id)})
     return student_id, 200
