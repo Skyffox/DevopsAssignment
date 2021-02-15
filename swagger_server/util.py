a06859184e02b500fb2ed9364c55653927ade4e8
Julius <julius.wagt1996@hotmail.com>
2021-02-15 13:21:32 +0100

    added change util py v2

--- swagger_server/util.py
+++ swagger_server/util.py
@@ -6,10 +6,8 @@ import typing
 
 def _deserialize(data, klass):
     """Deserializes dict, list, str into an object.
-
     :param data: dict, list or str.
     :param klass: class literal, or string of class name.
-
     :return: object.
     """
     if data is None:
@@ -22,21 +20,18 @@ def _deserialize(data, klass):
         return deserialize_date(data)
     elif klass == datetime.datetime:
         return deserialize_datetime(data)
-    elif hasattr(klass, '__origin__')::
-        if klass.__extra__ == list:
+    elif hasattr(klass, '__origin__'):
+        if klass.__origin__ == list:
             return _deserialize_list(data, klass.__args__[0])
-        if klass.__extra__ == dict:
+        if klass.__origin__ == dict:
             return _deserialize_dict(data, klass.__args__[1])
     else:
         return deserialize_model(data, klass)
 
-
 def _deserialize_primitive(data, klass):
     """Deserializes to primitive type.
-
     :param data: data to deserialize.
     :param klass: class literal.
-
     :return: int, long, float, str, bool.
     :rtype: int | long | float | str | bool
     """
@@ -51,7 +46,6 @@ def _deserialize_primitive(data, klass):
 
 def _deserialize_object(value):
     """Return a original value.
-
     :return: object.
     """
     return value
@@ -59,7 +53,6 @@ def _deserialize_object(value):
 
 def deserialize_date(string):
     """Deserializes string to date.
-
     :param string: str.
     :type string: str
     :return: date.
@@ -74,9 +67,7 @@ def deserialize_date(string):
 
 def deserialize_datetime(string):
     """Deserializes string to datetime.
-
     The string should be in iso8601 datetime format.
-
     :param string: str.
     :type string: str
     :return: datetime.
@@ -91,7 +82,6 @@ def deserialize_datetime(string):
 
 def deserialize_model(data, klass):
     """Deserializes list or dict to model.
-
     :param data: dict, list.
     :type data: dict | list
     :param klass: class literal.
@@ -114,11 +104,9 @@ def deserialize_model(data, klass):
 
 def _deserialize_list(data, boxed_type):
     """Deserializes a list and its elements.
-
     :param data: list to deserialize.
     :type data: list
     :param boxed_type: class literal.
-
     :return: deserialized list.
     :rtype: list
     """
@@ -128,11 +116,9 @@ def _deserialize_list(data, boxed_type):
 
 def _deserialize_dict(data, boxed_type):
     """Deserializes a dict and its elements.
-
     :param data: dict to deserialize.
     :type data: dict
     :param boxed_type: class literal.
-
     :return: deserialized dict.
     :rtype: dict
     """
