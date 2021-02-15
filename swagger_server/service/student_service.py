b0c2ba02fa88c673e1a93461a079215a484fef27
Julius <julius.wagt1996@hotmail.com>
2021-02-15 14:13:22 +0100

    version final v4

--- swagger_server/service/student_service.py
+++ swagger_server/service/student_service.py
@@ -16,7 +16,7 @@ student_db = TinyDB(db_file_path)
 
 
 def add_student(student):
-    if not student.first_name is None or student.last_name is None:
+    if student.first_name is None or student.last_name is None:
         return 'invalid input', 405
 
     queries = []
